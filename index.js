b9db8964d25e468a65fb4f64fc3b175eecd34c00
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-11-11 22:50:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 const companyName="Scuber";
-mostProfitableNeighborhood == "Chelsea";
-CompanyCeo == "Susan Smith";
+let mostProfitableNeighborhood = "Chelsea";
+let CompanyCeo = "Susan Smith";
